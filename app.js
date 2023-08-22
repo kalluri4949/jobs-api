f6ece3d561113bb11a3b0f50f0291edb9074ea8a
Rakesh Kalluri <kalluri.rakesh15@gmail.com>
2023-08-22 07:57:27 -0400

    swagger docs added

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
+//Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const express = require('express');
 const app = express();
 
@@ -35,6 +40,13 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
+
+//swagger use
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 //routes handler usage
 app.use('/api/v1/auth', authRouter);
